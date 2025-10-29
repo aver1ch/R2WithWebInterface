c346f8fb45376c308432ba055a1725b3052d0154
encore180 <astegosgrs@gmail.com>
2025-10-29 22:17:47 +0200

    Страница Login частично сделана

--- frontend/web_interface/src/pages/Login.tsx
+++ frontend/web_interface/src/pages/Login.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import '../styles/App.css'
 type Props = {}
 
 const Login = (props: Props) => {
+  //let login = "сигмен"
+  //let password = "федя";
   return (
-    <div>Login</div>
+    <div className='background-main'>
+        <div className="container">
+            <div className="rectangle-black"></div>
+        </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
